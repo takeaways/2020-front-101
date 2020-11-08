11ce47fb5282f9592a2b467c5d11badb02aa03f9
JangGeonil <wkdrjsdlf2@gmail.com>
2020-11-09 00:37:08 +0900

    project1 refactoring

--- project1-coordinates/main.js
+++ project1-coordinates/main.js
@@ -1,20 +1,28 @@
 const vertical = document.querySelector(".vertical");
 const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
-const tag = document.querySelector(".tag");
 
-document.addEventListener("mousemove", (event) => {
-  //window 위라서 client!!! 좌표 사용
-  const clientX = event.clientX;
-  const clientY = event.clientY;
+window.addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+  const tag = document.querySelector(".tag");
+  document.addEventListener("mousemove", (event) => {
+    //window 위라서 client!!! 좌표 사용
+    const clientX = event.clientX;
+    const clientY = event.clientY;
 
-  vertical.style.left = `${clientX}px`;
-  horizontal.style.top = `${clientY}px`;
-  target.style.left = `${clientX}px`;
-  target.style.top = `${clientY}px`;
-  tag.style.left = `${clientX}px`;
-  tag.style.top = `${clientY}px`;
-  tag.innerHTML = `
+    console.log(targetHalfHeight);
+
+    vertical.style.transform = `translateX(${clientX}px)`;
+    horizontal.style.transform = `translateY(${clientY}px)`;
+    target.style.transform = `translate(${clientX - targetHalfWidth}px,${
+      clientY - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${clientX}px,${clientY}px)`;
+
+    tag.innerHTML = `
     x:${clientX}, y:${clientY}
   `;
+  });
 });
